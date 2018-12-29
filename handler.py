11c40d073a89f7ead7b7fad7cf574dafd1a088fd
Gabriel Plassard <gabriel.plassard@gmail.com>
2018-12-29 17:41:33 +0100

    patch webhook

--- handler.py
+++ handler.py
@@ -3,18 +3,48 @@ import base64
 import json
 from botocore.exceptions import ClientError
 import cfn_resource
+import logging
+from urllib.request import urlopen, Request, HTTPError, URLError
 
 handler = cfn_resource.Resource()
+codebuild = boto3.client('codebuild')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 @handler.create
 @handler.update
 def get_credentials(event, context):
     try:
+        props = event['ResourceProperties']
+        deleteResponse = codebuild.delete_webhook(projectName=props['CodebuildProjectName'])
+        logger.info('delete response %s', deleteResponse)
+        createResponse = codebuild.create_webhook(projectName=props['CodebuildProjectName'])
+        logger.info('create response %s', createResponse)
+
+        webhookUrl = createResponse['webhook']['url']
+        payload = json.dumps({'events':['pull_request']})
+        token = props['GithubOAuth']
+        req = Request(
+            webhookUrl, 
+            method='PATCH',
+            data=payload.encode('utf-8'), 
+            headers={   
+                'Content-Length': len(payload), 
+                'Content-Type': 'application/json', 
+                'Authorization': 'token ' + token
+            }
+        )
+
+        response = urlopen(req)
+        logger.info('Github response %s', response)
+
         return {
             'Status': cfn_resource.SUCCESS,
-            'PhysicalResourceId': event['LogicalResourceId']
+            'PhysicalResourceId': webhookUrl
         }
     except Exception:
+        logger.exception('Unexpected error')
         return {
             'Status': cfn_resource.FAILED,
             'Reason': 'Unexpected error'
@@ -22,6 +52,9 @@ def get_credentials(event, context):
 
 @handler.delete
 def on_delete(event, context):
+    props = event['ResourceProperties']
+    deleteResponse = codebuild.delete_webhook(projectName=props['CodebuildProjectName'])
+    logger.info('delete response %s', deleteResponse)
     return {
         'Status': cfn_resource.SUCCESS
     }
